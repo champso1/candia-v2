1a6ba2f26201e3c20d3f5783103a039780897dc2
Casey Hampson <champson1864@gmail.com>
2026-01-10 22:23:31 -0500

    mid update

--- tests/testing/testing.cpp
+++ tests/testing/testing.cpp
@@ -1,13 +1,33 @@
-#include <vector>
-
+#include "Candia-v2/Distribution.hpp"
 #include "Candia-v2/Common.hpp"
-#include "Candia-v2/Grid.hpp"
+#include "LHAPDF/LHAGlue.h"
+#include <charconv>
+#include <cstring>
 using namespace Candia2;
 
-int main()
+static void usage()
 {
+	log(LOG_INFO, "testing.cpp", "USAGE: ./testing <pdf> <Q0> <x>");
+	log(LOG_INFO, "testing.cpp", "    <pdf>: name of LHAPDF set");
+	log(LOG_INFO, "testing.cpp", "    <Q0>: energy scale");
+	log(LOG_INFO, "testing.cpp", "    <x>: momentum fraction");
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4) {
+		log(LOG_ERROR_NOQUIT, "testing.cpp", "Invalid number of arguments: {}", argc);
+		usage();
+	}
+	LHAPDF::setVerbosity(0);
+
+	std::string setname(argv[1]);
+	double Q0{}, x{};
+	std::from_chars(argv[2], argv[2] + strlen(argv[2]), Q0);
+	std::from_chars(argv[3], argv[3] + strlen(argv[3]), x);
+
 	
-    getDebugFlag() = true;
-	std::vector<double> xtab{1e-5, 1e-4, 1e-3, 1e-2, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0};
-	Grid grid(xtab, 500, 50, 2);
+	auto pdf = LHAPDFDistribution::make_lhapdf_pdf(setname, 0);
+	pdf->
 }
