a06a28d0a25b7d331a3f00eced0c5ec2ff8c8a1f
Casey Hampson <champson1864@gmail.com>
2026-02-16 22:31:31 -0500

    removed extraneous include from math.cpp

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,5 +1,4 @@
 #include "Candia-v2/Math.hpp"
-#include "Candia-v2/Common.hpp"
 
 namespace Candia2
 {
@@ -10,5 +9,5 @@ namespace Candia2
 		while (x > 1.0)
 			res *= x--;
 		return res;
-	}	
+	}
 };
